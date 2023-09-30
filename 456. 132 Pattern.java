23f4cd8ad7c2011ef833800a445077d8db76b92b
Suraj Kumar Behera <101680569+Suraj1719@users.noreply.github.com>
2023-09-30 16:07:59 +0530

    Update 456. 132 Pattern.java

--- 456. 132 Pattern.java
+++ 456. 132 Pattern.java
@@ -1,3 +1,39 @@
+Brute Force : O(n^3)
+    class Solution {
+    public boolean find132pattern(int[] nums) {
+        //brute force - TLE :O(n^3)
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]<nums[k] && nums[k]<nums[j])
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+Better approach : O(n^2)
+
+    class Solution {
+    public boolean find132pattern(int[] nums) {
+        
+        int num_i=nums[0];
+        int n=nums.length;
+        for(int j=1;j<n-1;j++){
+            num_i=Math.min(num_i,nums[j]);
+            for(int k=j+1;k<n;k++){
+                if(num_i<nums[k] && nums[k]<nums[j])return true;
+            }
+        }
+        return false;
+    }
+}
+
+//Best approach : (stack):O(n)
+
 class Solution {
     public boolean find132pattern(int[] nums) {
         //Stack & Left Approach
