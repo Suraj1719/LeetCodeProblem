f7c9b78cf6ec3d44fad6a80146aea3851fbb8219
Suraj Kumar Behera <101680569+Suraj1719@users.noreply.github.com>
2023-11-10 06:58:52 +0530

    Time: 97 ms (77.05%), Space: 80.4 MB (97.13%) - LeetHub

--- 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.java
+++ 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.java
@@ -1,8 +1,57 @@
-class Solution {
+// class Solution {
 
-    Map<Integer, List<Integer>> graph = new HashMap();
+//     Map<Integer, List<Integer>> graph = new HashMap();
+//     public int[] restoreArray(int[][] adjacentPairs) {
+//         //dfs traversal   time :O(n)   space : O(n)
+//         for (int[] edge : adjacentPairs) {
+//             int x = edge[0];
+//             int y = edge[1];
+            
+//             if (!graph.containsKey(x)) {
+//                 graph.put(x, new ArrayList());
+//             }
+            
+//             if (!graph.containsKey(y)) {
+//                 graph.put(y, new ArrayList());
+//             }
+            
+//             graph.get(x).add(y);
+//             graph.get(y).add(x);
+//         }
+        
+//         int root = 0;
+//         for (int num : graph.keySet()) {
+//             if (graph.get(num).size() == 1) {
+//                 root = num;
+//                 break;
+//             }
+//         }
+        
+//         int[] ans = new int[graph.size()];
+
+//         dfs(root, Integer.MAX_VALUE, ans, 0);//cur , parent , ansarray , index
+
+//         return ans;
+//     }
+
+//     private void dfs(int node, int prev, int[] ans, int i) {
+//         ans[i] = node;
+//         for (int neighbor : graph.get(node)) {
+//             if (neighbor != prev) {
+//                 dfs(neighbor, node, ans, i + 1);
+//             }
+//         }
+//     }
+// }
+
+
+
+//Approach 2: Iterative, Follow the Path
+//time :O(n)  spae :O(n)
+class Solution {
     public int[] restoreArray(int[][] adjacentPairs) {
-        //dfs traversal   time :O(n)   space : O(n)
+        Map<Integer, List<Integer>> graph = new HashMap();
+        
         for (int[] edge : adjacentPairs) {
             int x = edge[0];
             int y = edge[1];
@@ -27,19 +76,26 @@ class Solution {
             }
         }
         
+        int curr = root;
         int[] ans = new int[graph.size()];
-
-        dfs(root, Integer.MAX_VALUE, ans, 0);//cur , parent , ansarray , index
+        ans[0] = root;
+        int i = 1;
+        int prev = Integer.MAX_VALUE;
+        
+        while (i < graph.size()) {
+            for (int neighbor : graph.get(curr)) {
+                if (neighbor != prev) {
+                    ans[i] = neighbor;
+                    i++;
+                    prev = curr;
+                    curr = neighbor;
+                    break;
+                }
+            }
+        }
 
         return ans;
     }
-
-    private void dfs(int node, int prev, int[] ans, int i) {
-        ans[i] = node;
-        for (int neighbor : graph.get(node)) {
-            if (neighbor != prev) {
-                dfs(neighbor, node, ans, i + 1);
-            }
-        }
-    }
 }
+
+
