ca76436e39ab9ed74a5c1cd06ef8b4aa307713a9
Suraj Kumar Behera <beherasuraj440@gmail.com>
2024-06-12 11:12:50 +0530

    Time: 0 ms (100%), Space: 41.8 MB (42.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -5,25 +5,55 @@ class Solution {
         //Arrays.sort(nums);
 
         //Approach - 2 : counting
-        int zero=0;
-        int one=0;
-        int two=0;
-        for(int x:nums){
-            if(x==0)zero++;
-            else if(x==1)one++;
-            else two++;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(zero>0){
-                nums[i]=0;
-                zero--;
-            }else if(one>0){
-                nums[i]=1;
-                one--;
-            }else{
-                nums[i]=2;
-                two--;
+        // int zero=0;
+        // int one=0;
+        // int two=0;
+        // for(int x:nums){
+        //     if(x==0)zero++;
+        //     else if(x==1)one++;
+        //     else two++;
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     if(zero>0){
+        //         nums[i]=0;
+        //         zero--;
+        //     }else if(one>0){
+        //         nums[i]=1;
+        //         one--;
+        //     }else{
+        //         nums[i]=2;
+        //         two--;
+        //     }
+        // }
+
+        //Approach -3 : In place 
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        int temp;
+        while(mid<=high){
+            switch(nums[mid]){
+                case 0:
+                    {
+                    temp=nums[mid];
+                    nums[mid]=nums[low];
+                    nums[low]=temp;
+                    low++;mid++;
+                    break;
+                    }
+                case 1:{
+                    mid++;
+                    break;
+                }
+                case 2:{
+                    temp=nums[mid];
+                    nums[mid]=nums[high];
+                    nums[high]=temp;
+                    high--;
+                    break;
+                }
             }
+            
         }
     }
 }
