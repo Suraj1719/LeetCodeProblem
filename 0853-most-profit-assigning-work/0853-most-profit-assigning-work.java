f774aa844111b1921ee10d3ce4e76c9983e4d7ce
Suraj Kumar Behera <beherasuraj440@gmail.com>
2024-06-19 17:58:25 +0530

    Time: 29 ms (32.52%), Space: 45.5 MB (68.2%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -4,15 +4,34 @@ class Solution {
         int n=difficulty.length;
         int m=worker.length;
         int ans=0;
-        for(int i=0;i<m;i++){
-            int cur_max_pro=0;
-            for(int j=0;j<n;j++){
-                if(difficulty[j]<=worker[i]){
-                    cur_max_pro=Math.max(cur_max_pro,profit[j]);
-                }
-            }
-            ans+=cur_max_pro;
+        // for(int i=0;i<m;i++){
+        //     int cur_max_pro=0;
+        //     for(int j=0;j<n;j++){
+        //         if(difficulty[j]<=worker[i]){
+        //             cur_max_pro=Math.max(cur_max_pro,profit[j]);
+        //         }
+        //     }
+        //     ans+=cur_max_pro;
+        // }
+
+        //Approach -2 Maximum Heap and sorting : time= O(log(n))+O(max(n,m))  space:O(n)
+        PriorityQueue<int[]>max_heap=new PriorityQueue<>((a,b)->b[0]-a[0]);
+        for(int i=0;i<n;i++){
+            max_heap.add(new int[]{profit[i],difficulty[i]});
         }
+        Arrays.sort(worker);
+        int ind=m-1;
+        while(ind>=0 && !max_heap.isEmpty()){
+            int work=worker[ind];
+            int cur[]=max_heap.peek();
+            if(work<cur[1]){
+                max_heap.poll();
+            }else{
+                ans+=cur[0];
+                ind--;
+            }
+        }
+
         return ans;
     }
 }
