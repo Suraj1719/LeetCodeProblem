0142bdc6cb4752315f7929bfc995d51c999643d9
Suraj Kumar Behera <beherasuraj440@gmail.com>
2024-10-09 07:57:34 +0530

    Time: 12 ms (99.07%), Space: 56.1 MB (8.59%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -2,21 +2,38 @@ class Solution {
     public int minSwaps(String s) {
         //Algorithm - 1 (Counting Imbalance) Stack
         int len=s.length();
-        Stack<Character>st=new Stack();
-        int imbalance_count=0;
+        // Stack<Character>st=new Stack();
+        // int imbalance_count=0;
+        // for(char c:s.toCharArray()){
+        //     if(c=='['){
+        //         st.push(c);
+        //     }else{
+        //         if(st.isEmpty()){
+        //             imbalance_count++;
+        //         }else{
+        //             st.pop();
+        //         }
+        //     }
+        // }
+
+        // int imb_pair=(imbalance_count+st.size())/2;
+        // return (imb_pair+1)/2;
+
+        //Algorithm - 2 (closing balance overbalance)
+        //closing bracket count must be <= opening bracket count
+        //time :O(n)  space:O(1)
+
+        int closing=0;//at the end it always '0' as equal number of open and close
+        int max_closing=0;
+
         for(char c:s.toCharArray()){
             if(c=='['){
-                st.push(c);
+                closing--;
             }else{
-                if(st.isEmpty()){
-                    imbalance_count++;
-                }else{
-                    st.pop();
-                }
+                closing++;
+                max_closing=Math.max(closing,max_closing);
             }
         }
-
-        int imb_pair=(imbalance_count+st.size())/2;
-        return (imb_pair+1)/2;
+        return (max_closing+1)/2;
     }
 }
