e8e5825bd8ed5d8e630dbbb77caadf3e45f50ac8
Suraj Kumar Behera <beherasuraj440@gmail.com>
2024-10-28 11:16:50 +0530

    Time: 45 ms (62.5%), Space: 57.7 MB (84.72%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
@@ -1,29 +1,57 @@
 class Solution {
+
     public int longestSquareStreak(int[] nums) {
-        //Arrays.sort(nums);
-        int max_pos_len=-1;
-        int len=nums.length;
-        Set<Long>vis=new HashSet<>();
-        Set<Long>ele=new HashSet<>();
-        for(int e:nums){
-            ele.add(e*1l);
-        }
-        for(int i=0;i<len;i++){  //O(n)
-            long temp=nums[i]*1l;
-            //System.out.println(temp);
-            if(vis.contains(temp))continue;
-            vis.add(temp);
-            int cur_size=0;
-            while(ele.contains(temp*temp)){ //O(log n)
-                cur_size++;
-                System.out.println("inside "+temp);
-                temp=temp*temp;
-                System.out.println("inside 2 ="+temp);
-                vis.add(temp);
-                max_pos_len=Math.max(max_pos_len,cur_size+1);
+        //Approach 1: Binary Search
+
+        //time :O(n logn)  space:O(n)
+        Arrays.sort(nums);
+        // Set to keep track of numbers we've already processed
+        Set<Integer> processedNumbers = new HashSet<>();
+        int longestStreak = 0;
+
+        // Iterate through each number in the sorted array
+        for (int current : nums) {
+            // Skip if we've already processed this number
+            if (processedNumbers.contains(current)) continue;
+
+            int streak = current;
+            int streakLength = 1;
+
+            // Continue the streak as long as we can find the square of the current number
+            while (true) {
+                // Break if the square would be larger than 10^5 (problem constraint)
+                if ((long) streak * (long) streak > 1e5) break;
+                // If we find the square, continue the streak
+                if (binarySearch(nums, streak * streak)) {
+                    streak *= streak;
+                    processedNumbers.add(streak);
+                    streakLength++;
+                } else {
+                    break;
+                }
             }
-            
+            // Update the longest streak if necessary
+            longestStreak = Math.max(longestStreak, streakLength);
         }
-        return max_pos_len;
+
+        // Return -1 if no valid streak found, otherwise return the longest streak
+        return longestStreak < 2 ? -1 : longestStreak;
+    }
+
+    // Binary search helper function to efficiently find a value in the sorted array
+    private boolean binarySearch(int[] nums, int target) {
+        if (target < 0) return false;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return false;
     }
 }
