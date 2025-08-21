8d3691eef5762e0a2847a16b553f84b43eeeda0d
Suraj Kumar Behera <beherasuraj440@gmail.com>
2025-08-21 11:28:43 +0530

    Time: 116 ms (5.14%), Space: 46 MB (28.08%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
@@ -1,28 +1,46 @@
 class Solution {
-
     public int numSubmat(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int res = 0;
-        int[][] row = new int[m][n];
+        int row=mat.length;
+        int col=mat[0].length;
+        int ans=0;
+        //from every index having 1, we tend to fix that position
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j == 0) {
-                    row[i][j] = mat[i][j];
-                } else {
-                    row[i][j] = mat[i][j] == 0 ? 0 : row[i][j - 1] + 1;
-                }
-                int cur = row[i][j];
-                for (int k = i; k >= 0; k--) {
-                    cur = Math.min(cur, row[k][j]);
-                    if (cur == 0) {
-                        break;
-                    }
-                    res += cur;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j]==1){
+                    ans+=help(mat,i,j);
                 }
             }
         }
-        return res;
+        return ans;
+    }
+
+    int help(int mat[][],int r,int c){
+        int row=mat.length;
+        int col=mat[0].length;
+        int poss_tri=0;
+
+        int max_pos_width=col;
+
+        int cur_width=0;
+
+        for(int i=r;i<row;i++){
+
+            if(mat[i][c]==0){
+                break;
+            }
+            cur_width=0;
+            for(int j=c;j<max_pos_width;j++){
+                if(mat[i][j]==0){
+                    max_pos_width=j;
+                    break;
+                }
+                cur_width+=1;
+            }
+
+            max_pos_width=Math.min(max_pos_width,c+cur_width);
+            poss_tri+=(max_pos_width-c);
+        }
+        return poss_tri;
     }
 }
