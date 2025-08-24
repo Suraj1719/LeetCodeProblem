ea8e0d3af55cc40681f1c25e9a11d71d87c0bd26
Suraj Kumar Behera <beherasuraj440@gmail.com>
2025-08-24 08:24:24 +0530

    Time: 3 ms (77.92%), Space: 57.3 MB (17.61%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,38 +1,67 @@
+// class Solution {
+//     public int longestSubarray(int[] nums) {
+//         int len=nums.length;
+//         boolean one=false;
+//         int pre_con_one[]=new int[len];
+//         int con_one=0;
+
+//         for(int i=0;i<len;i++){
+//             if(nums[i]==1){
+//                 con_one++;
+//                 one=true;
+//             }else{
+//                 pre_con_one[i]=con_one;
+//                 con_one=0;
+//             }
+//         }
+//         int post_con_one[]=new int[len+1];
+//         con_one=0;
+
+//         for(int i=len-1;i>=0;i--){
+//             if(nums[i]==1){
+//                 con_one++;
+//             }else{
+//                 post_con_one[i]=con_one;
+//                 con_one=0;
+//             }
+//         }
+
+//         int ans=0;
+//         for(int i=0;i<len;i++){
+//             ans=Math.max(ans,post_con_one[i]+pre_con_one[i]);
+//         }
+
+//         if(ans==0 && one==true)ans=len-1;
+
+//         return ans;
+//     }
+// }
+
+
+
+//approach - 2 sliding window
+
 class Solution {
     public int longestSubarray(int[] nums) {
-        int len=nums.length;
-        boolean one=false;
-        int pre_con_one[]=new int[len];
-        int con_one=0;
-
-        for(int i=0;i<len;i++){
-            if(nums[i]==1){
-                con_one++;
-                one=true;
-            }else{
-                pre_con_one[i]=con_one;
-                con_one=0;
-            }
-        }
-        int post_con_one[]=new int[len+1];
-        con_one=0;
-
-        for(int i=len-1;i>=0;i--){
-            if(nums[i]==1){
-                con_one++;
-            }else{
-                post_con_one[i]=con_one;
-                con_one=0;
-            }
-        }
-
-        int ans=0;
-        for(int i=0;i<len;i++){
-            ans=Math.max(ans,post_con_one[i]+pre_con_one[i]);
-        }
-
-        if(ans==0 && one==true)ans=len-1;
+        // Number of zero's in the window.
+        int zeroCount = 0;
+        int longestWindow = 0;
+        // Left end of the window.
+        int start = 0;
         
-        return ans;
+        for (int i = 0; i < nums.length; i++) {
+            zeroCount += (nums[i] == 0 ? 1 : 0);
+                          
+            // Shrink the window until the count of zero's
+            // is less than or equal to 1.
+            while (zeroCount > 1) {
+                zeroCount -= (nums[start] == 0 ? 1 : 0);
+                start++;
+            }
+              
+            longestWindow = Math.max(longestWindow, i - start);
+        }
+
+        return longestWindow;
     }
 }
