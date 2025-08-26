7fcbc6424f40d1f085ca2d28da7818387c737d96
Suraj Kumar Behera <beherasuraj440@gmail.com>
2025-08-26 08:47:03 +0530

    Time: 1 ms (24.26%), Space: 44.1 MB (94.78%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        //Brute force approach
+        // If the tree is empty, it's balanced
+        if (root == null) {
+            return true;
+        }
+
+        // Calculate the height of left and right subtrees
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        // Check if the absolute difference in heights
+        // of left and right subtrees is <= 1
+        if (Math.abs(leftHeight - rightHeight) <= 1 &&
+            isBalanced(root.left) &&
+            isBalanced(root.right)) {
+            return true;
+        }
+
+        // If any condition fails, the tree is unbalanced
+        return false;
+
+    }
+
+    // Function to calculate the height of a subtree
+    public int getHeight(TreeNode root) {
+        // Base case: if the current node is NULL,
+        // return 0 (height of an empty tree)
+        if (root == null) {
+            return 0;
+        }
+
+        // Recursively calculate the height
+        // of left and right subtrees
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        // Return the maximum height of left and right subtrees
+        // plus 1 (to account for the current node)
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
