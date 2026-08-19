19425358ab47c79909b721396d2caccff0b2f585
Suraj Kumar Behera <beherasuraj440@gmail.com>
2026-08-19 15:04:00 +0530

    Time: 29 ms (41.32%), Space: 55.4 MB (20%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,38 +1,37 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        // Map row number to its bitmask representing reserved seats
-        Map<Integer, Integer> rowToMask = new HashMap<>();
-        
-        for (int[] seat : reservedSeats) {
-            int row = seat[0];
-            int col = seat[1];
-            // We only care about seats 2 to 9
-            if (col >= 2 && col <= 9) {
-                int currentMask = rowToMask.getOrDefault(row, 0);
-                rowToMask.put(row, currentMask | (1 << col));
-            }
+        Map<Integer,Set<Integer>> rowtoNum=new HashMap<>();
+        // for(int i=1;i<=n;i++){
+        //     rowtoNum.put(i,new HashSet<Integer>());
+        // }
+
+        for(int i=0;i<reservedSeats.length;i++){
+            int r=reservedSeats[i][0];
+            int num=reservedSeats[i][1];
+
+            if(num>=2 && num<=9)
+            rowtoNum.computeIfAbsent(r, k -> new HashSet<>()).add(num);
+
         }
-        
-        // Assume 2 families for all completely empty rows
-        int totalFamilies = (n - rowToMask.size()) * 2;
-        
-        // Define our target masks
-        int leftMask = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
-        int rightMask = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
-        int middleMask = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
-        
-        for (int mask : rowToMask.values()) {
-            boolean leftFree = (mask & leftMask) == 0;
-            boolean rightFree = (mask & rightMask) == 0;
-            boolean middleFree = (mask & middleMask) == 0;
+
+        int ans=2*(n-rowtoNum.size());
+
+        for (Set<Integer> reserved : rowtoNum.values()) {
+            boolean leftBlocked = reserved.contains(2) || reserved.contains(3) || 
+                                    reserved.contains(4) || reserved.contains(5);
+            boolean rightBlocked = reserved.contains(6) || reserved.contains(7) || 
+                                     reserved.contains(8) || reserved.contains(9);
+            boolean middleBlocked = reserved.contains(4) || reserved.contains(5) || 
+                                     reserved.contains(6) || reserved.contains(7);
             
-            if (leftFree && rightFree) {
-                totalFamilies += 2;
-            } else if (leftFree || rightFree || middleFree) {
-                totalFamilies += 1;
+            if (!leftBlocked && !rightBlocked) {
+                ans += 2;
+            } else if (!leftBlocked || !rightBlocked || !middleBlocked) {
+                ans += 1;
             }
+            // If all are blocked, we add 0
         }
-        
-        return totalFamilies;
+
+        return ans;
     }
 }
